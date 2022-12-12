fd5a4d5c9e9a06e11c3fc3b06f8dd29e7875bdf5
mikaelelg <mikael.elg.87@gmail.com>
2022-12-12 23:26:51 +0100

    Namechanges and change in example file

--- backend/database/migrations/2022_12_08_145404_create_whishlists_table.php
+++ backend/database/migrations/2022_12_08_145404_create_whishlists_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('userID');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
